29beb07d6e3afe8f7ce46c40333fce28f287c89c
Kovyazin Andrey <hiroazora@gmail.com>
2024-12-10 21:04:09 +0500

    Task5

--- Tyuiu.KovyazinAA.Sprint6.Task5.V1.Test/DataServiceTest.cs
+++ Tyuiu.KovyazinAA.Sprint6.Task5.V1.Test/DataServiceTest.cs
@@ -9,7 +9,10 @@ namespace Tyuiu.KovyazinAA.Sprint6.Task5.V1.Test
         {
             DataService ds = new DataService();
             string path = @"C:\DataSprint6\InPutDataFileTask5V1.txt";
-
+            FileInfo fileInfo = new FileInfo(path);
+            bool file = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, file);
         }
     }
 }
